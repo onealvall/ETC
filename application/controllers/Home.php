f94912dbc97914ad0d4b9c3fa80af4918058809b
onealvall <39580409+onealvall@users.noreply.github.com>
2024-10-06 00:44:35 +0800

    updated

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,19 +15,6 @@ class Home extends CI_Controller
 		$this->load->view('includes/header');
 		$this->load->view('includes/navigation');
 		$this->load->view('sections/front');
-		$this->load->view('sections/services-rework');
-		// $this->load->view('sections/services');
-		$this->load->view('sections/our_competitive_advantage');
-		$this->load->view('sections/whoweare');
-		$this->load->view('sections/mission_vission');
-		// $this->load->view('sections/portfolio');
-		$this->load->view('sections/team');
-		$this->load->view('sections/careers');
-		$this->load->view('sections/facilities');
-		// $this->load->view('sections/headquarters');
-		// $this->load->view('sections/clients');
-		// $this->load->view('sections/contact');
-		// $this->load->view('includes/footer');
 		$this->load->view('includes/contact');
 	}
 }
--- application/views/sections/contact copy.php
+++
@@ -1,8 +0,0 @@
-        <!-- Contact-->
-        <section class="page-section p-0" id="contact" style="">
-            <div class="container p-4">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase mb-0">Contact Us</h2>
-                </div>
-            </div>
-        </section>
