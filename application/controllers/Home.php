5ab5c730a8fcc8764600cb60c34815a2dc31df74
onealvall <39580409+onealvall@users.noreply.github.com>
2024-10-06 11:52:16 +0800

    updated

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,6 +18,9 @@ class Home extends CI_Controller
 		$this->load->view('sections/we_have_trusted');
 		$this->load->view('sections/about_us');
 		$this->load->view('sections/personal_account_services');
+		$this->load->view('sections/confidental_data');
+		$this->load->view('sections/accelarated_growth_low_costs');
+		$this->load->view('sections/best_reasons');
 		$this->load->view('sections/expand_your_next');
 		$this->load->view('sections/contact');
 		$this->load->view('includes/contact');
---
+++ application/views/sections/accelarated_growth_low_costs.php
@@ -0,0 +1,23 @@
+<section class="page-section vertical-center-content no-bg-color text-white" id="careers" style="height: 100vh;">
+    <div class="container content-above">
+        <div class="row">
+           <div class="col-md-8">
+               <h1>ACCELERATED GROWTH</h1>
+               <p>Our dedicated team will allow you to focus on strategic planning in growing and serving your customers which will save you time and money of having to worry about your back-office needs.</p>
+            </div>
+            <div class="col-md-4">
+                <img src="<?php echo base_url() ?>assets/img/accelarated_growth_low_costs/1.png" class="card-img-top" alt="...">
+            </div>
+        </div>
+        <div class="row">
+           <div class="col-md-8">
+           <h1>LOW COSTS ON STAFFING</h1>
+           <p>Drives operational budget at its peak by outsourcing our global dedicated Accountants, complementing your team's local equivalents which will reduce staffing costs of up to 68% savings to account more on your firm's profitability net worth to service more of your customer's needs.</p>
+            </div>
+            <div class="col-md-4">
+                 <img src="<?php echo base_url() ?>assets/img/accelarated_growth_low_costs/2.png" class="card-img-top" alt="...">
+             </div>
+        </div>
+    </div>
+    <div class="front-overlay"></div>
+</section>
